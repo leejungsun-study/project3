87b8ca324a4b5b7964d5858bea4638140a459d42
jungsun <skyljs@gmail.com>
2024-12-03 13:36:02 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+import {PicList} from './data.js';
 import './App.css';
+import {useState} from 'react';
+
 
 function App() {
+   const [index, setIndex] = useState(0);
+   const [show, setShow] =useState(false);
+
+   function handleClick() {
+    index>=5 ? setIndex(index-5) : setIndex(index+1)
+   }
+
+   function handleShow(){
+      setShow(!show)
+   }
+
+  let Pic =  PicList[index]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <div id="wrap">
+        <h1>데이터 배열객체 불러오기 </h1>
+        <hr></hr>
+        <button onClick={handleClick}>next</button>
+         <div className='box'>
+          <h2>현재갤러리 번호- {index+1}/ 전체 갤러리 수 {PicList.length}</h2>  
+         <h2 className="blue"> 작품명 : {Pic.name}</h2>
+         <h2 className="right">작가명 : {Pic.artist}</h2>
+         <h2>작품 설명</h2>
+         <button onClick={handleShow}>작품 설명 {show ? "감추라" : "보여라" }</button>          {show && <p>{Pic.description} </p> }
+         <h2>작품 사진 이미지</h2>
+            <img src= {Pic.url} alt={Pic.alt}></img>
+         </div>
+        
+        
+      
+
+     
+    
+       
+     </div>
   );
 }
 
